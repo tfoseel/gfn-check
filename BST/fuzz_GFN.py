f01f314bd48ea506fdc3f0e551eb7ab4768ff03d
MisakiTaro0414 <ayhansuleymanzade@kaist.ac.kr>
2024-12-08 02:06:41 +0900

    fix fuzzer of local search

--- BST/fuzz_GFN.py
+++ BST/fuzz_GFN.py
@@ -43,7 +43,7 @@ def generate_tree(oracle, depth=0, pruning=True):
     return tree, num_nodes, tree.valid()
 
 
-def fuzz(oracle, unique_valid=1, valid=1, invalid=0):
+def fuzz(oracle, unique_valid=1, valid=1, invalid=0, local_search=False, local_search_steps=None):
     valids = 0
     print("Starting!", file=sys.stderr)
     valid_set = set()
@@ -54,6 +54,14 @@ def fuzz(oracle, unique_valid=1, valid=1, invalid=0):
         tqdm.write("{} trials, {} valids, {} unique valids, {} unique invalids, {:.2f}% unique valids".format(
             i, valids, len(valid_set), len(invalid_set), len(valid_set) * 100 / valids if valids != 0 else 0), end='\r')
         tree, num_nodes, validity = generate_tree(oracle)
+        if local_search:
+            for i in range(local_search_steps):
+                oracle.choice_sequence = oracle.choice_sequence[:len(oracle.choice_sequence)//2]
+                depth = oracle.calculate_depth()
+                new_tree, new_num_nodes, new_validity = generate_tree(oracle, depth)
+                if validity and tree.__repr__() not in valid_set:
+                    tree, num_nodes, validity = new_tree, new_num_nodes, new_validity
+                    break
         tqdm.write("Tree with {} nodes".format(num_nodes))
         if validity:
             tqdm.write("\033[0;32m" + tree.__repr__() + "\033[0m")
@@ -92,5 +100,5 @@ if __name__ == '__main__':
 
     oracle_g = GFNOracle_local_search(
         128, 128, [(VALUES, 1), (LEFT, 2), (RIGHT, 3)])
-    fuzz(oracle_g, unique_valid=1, valid=1, invalid=10e-20)
+    fuzz(oracle_g, unique_valid=1, valid=1, invalid=10e-20, local_search=True, local_search_steps=5)
     
